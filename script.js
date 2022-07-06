3d0a70db641051e7de2586526ea5c92011d851a0
Mcbryant14 <mcbryant35@gmail.com>
2022-07-06 19:49:55 -0400

    create pages

--- script.js
+++ script.js
@@ -5,3 +5,15 @@
 $('.cta').click(function() {
     alert("Contact information coming soon!");
 });
+
+// The logo image.
+let logoImage = document.querySelector('img');
+
+logoImage.onmouseenter = function() {
+    logoImage.src = 'Images/Site Icon.png';
+}
+
+logoImage.onmouseleave = function() {
+    logoImage.src = 'Images/logo.png';
+}
+
