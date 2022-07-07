9b376151bf9f18a3103877c77f9d28c76def454f
Mcbryant14 <mcbryant35@gmail.com>
2022-07-07 16:49:32 -0400

    add showDetails function to homepage

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ const jokes = [
     "My wife said I should do lunges to stay in shape. That would be a big step forward.",
     "Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.",
     "I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along.",
-    "Have you heard about the chocolate record player? It sounds pretty sweet."
+    "Have you heard about the chocolate record player? It sounds pretty sweet.",
+    "What’s brown and sticky? A stick.",
+    "Why are elevator jokes so classic and good? They work on many levels.",
+    "What did the police officer say to his belly-button? You’re under a vest.",
+    "What do you call it when a group of apes starts a company? Monkey business.",
+    "What kind of drink can be bitter and sweet? Reali-tea.",
+    "Want to know why nurses like red crayons? Sometimes they have to draw blood."
 ];
 
 function getRandomJoke(){
@@ -21,13 +27,14 @@ function getRandomJoke(){
     el.innerHTML = joke;
 }
 
-/*function generateNumber(jokes){
-    //gets random index value
-        const number = Math.floor(Math.random() * (jokes.length));
-        const item = jokes[number];
-        return item;
-      }
 
-function generateJoke(){
-    console.log(jokes[generateNumber();]);
-}*/
+function showDetails(){
+    var x = document.getElementById('hiddenDetails');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
