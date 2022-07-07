3b6ea383910df5d9f66842ad29913c9ac3bafdd9
Mcbryant14 <mcbryant35@gmail.com>
2022-07-07 13:41:29 -0400

    create dad joke generator

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-/*document.querySelector('.cta').addEventListener('click', function() {
-    alert("Contact information coming soon!");
-});*/
-
-$('.cta').click(function() {
+document.querySelector('.cta').addEventListener('click', function() {
     alert("Contact information coming soon!");
 });
 
-// The logo image.
-let logoImage = document.querySelector('img');
+/*Joke Generator*/
 
-logoImage.onmouseenter = function() {
-    logoImage.src = 'Images/Site Icon.png';
+const jokes = [
+    "I'm afraid for the calendar. Its days are numbered.",
+    "My wife said I should do lunges to stay in shape. That would be a big step forward.",
+    "Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.",
+    "I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along.",
+    "Have you heard about the chocolate record player? It sounds pretty sweet."
+];
+
+function getRandomJoke(){
+    //gets random index value
+    let number = Math.floor(Math.random() * (jokes.length));
+    //get array value
+    let joke = jokes[number];
+    const el = document.querySelector(".joke-output");
+    el.innerHTML = joke;
 }
 
-logoImage.onmouseleave = function() {
-    logoImage.src = 'Images/logo.png';
-}
+/*function generateNumber(jokes){
+    //gets random index value
+        const number = Math.floor(Math.random() * (jokes.length));
+        const item = jokes[number];
+        return item;
+      }
 
+function generateJoke(){
+    console.log(jokes[generateNumber();]);
+}*/
